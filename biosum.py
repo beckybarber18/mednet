cc3eb0c35859494053d78c37654c067182dadc64
Rebecca <rb25@princeton.edu>
2018-12-21 23:53:17 -0500

    Update biosum.py

--- biosum.py
+++ biosum.py
@@ -310,13 +310,13 @@ def details():
 def notFound(error):
     return 'Not Found'
 
-# if __name__ == '__main__':
-    # if len(argv) != 2:
-    #     print('Usage: ' + argv[0] + ' port')
-    #     exit(1)
+if __name__ == '__main__':
+    if len(argv) != 2:
+        print('Usage: ' + argv[0] + ' port')
+        exit(1)
 
-    # if not argv[1].isdigit():
-    #     print('Usage: port not an int')
-    #     exit(1)
+    if not argv[1].isdigit():
+        print('Usage: port not an int')
+        exit(1)
 
-    # run(host='0.0.0.0', port=argv[1], debug=True)
+    run(host='0.0.0.0', port=argv[1], debug=True)
